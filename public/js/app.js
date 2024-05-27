fdd04408b48c3b098c2ffb6320c5c8872695b15e
lxzanaa <zari.weone2010@gmail.com>
2024-05-27 19:13:01 +0300

    end

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,14 @@ mobile_open.addEventListener('click', function () {
 mobile_close.addEventListener('click', function () {
     mobile_menu.classList.toggle("left-0")
 })
+
+let header_link = document.querySelectorAll("#header_link")
+
+header_link.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        header_link.forEach(function (element, index) {
+            element.classList.remove('header_active')
+        })
+        item.classList.add("header_active");
+    })
+})
